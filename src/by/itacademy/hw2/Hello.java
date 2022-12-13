bcc725b6c0e21d7005ee87576cca2f42533b7c98
Ruslan <ruslan.tretyak.94@gmail.com>
2022-12-14 02:22:49 +0300

    remove files Hello

--- src/by/itacademy/hw2/Hello.java
+++ src/by/itacademy/hw2/Hello.java
@@ -1,5 +1,8 @@
+package by.itacademy.hw2;
+
 public class Hello {
    public static void main(String[] args) {
-      System.out.println("Hello World"); 
+
+      System.out.println("Hello World");
    }
 }
