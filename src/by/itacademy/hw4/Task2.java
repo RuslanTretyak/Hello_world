ad7c202f4df81d2a10528759f7804beb837eba54
Ruslan <ruslan.tretyak.94@gmail.com>
2022-12-21 23:49:56 +0300

    correcded Task2

--- src/by/itacademy/hw4/Task2.java
+++ src/by/itacademy/hw4/Task2.java
@@ -7,14 +7,17 @@ public class Task2 {
     public static void main(String[] args) {
 
         int sum = 0;
-        int prod = 1;
+        int prod = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите число");
         int a = sc.nextInt();
+        if (a != 0) {
+            prod = 1;
+        }
 
-        for(int i = a; i > 0; i /= 10) {
-           sum += i % 10;
-           prod *= (i % 10);
+        for (int i = a; i > 0; i /= 10) {
+            sum += i % 10;
+            prod *= (i % 10);
         }
         System.out.println("сумма цифр числа " + a + " равна " + sum);
         System.out.println("произведение цифр числа " + a + " равно " + prod);
